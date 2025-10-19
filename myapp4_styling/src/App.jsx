edb190e7d01ebab240eba365b691cdf594463c56
Shivam14raj <shivamrraj2005@gmail.com>
2025-10-19 11:28:30 +0530

    this folder is about styling in react using css

---
+++ myapp4_styling/src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  // styling as a object(line 17)
+  const cardStyle={
+           border:"1px solid #d6d4d4cc",
+           width:"200px", 
+           boxShadow:"1px 2px 3px 0px #c4c2c257"
+} 
+
+  return (  
+    <div style={{display:"flex", flexWrap:"wrap"}}>
+      <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+
+    <div>
+        <div style={cardStyle}> 
+            
+          <img style={{width:"200px"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="profile-img" />
+          <div style={{padding:"3px", margin:"10px"}}>
+            <h4>Rahul Kumar</h4>
+            <p>Software eng. </p>  
+          </div>
+        </div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default App
